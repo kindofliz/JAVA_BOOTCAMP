af5f520ce5278574b9d8e01c5be2f4abfa1de2b4
kindofliz <simanovicha@gmail.com>
2021-11-17 21:27:01 +0200

    added prepared statement example to sqlite code as well

--- src/lesson23/Lesson23SQLiteMain.java
+++ src/lesson23/Lesson23SQLiteMain.java
@@ -1,8 +1,10 @@
 package lesson23;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 
 public class Lesson23SQLiteMain {
@@ -19,6 +21,21 @@ public class Lesson23SQLiteMain {
             while (rs.next()) {
                 System.out.println("SQLITE: " + rs.getString("name"));
             }
+
+            //INSERT EXAMPLE
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter spaceship name: ");
+            String name = scanner.nextLine();
+
+            //!!!!
+            //INSTEAD OF REGULAR STATEMENT YOU SHOULD ALWAYS USE PreparedStatement
+            String preparedQuery = "INSERT INTO spaceships (type, active, name) VALUES ('regular', 1, ? )";
+            PreparedStatement preparedStatement = sqlconn.connection.prepareStatement(preparedQuery);
+
+            preparedStatement.setString(1, name);
+
+            preparedStatement.execute();
+            //!!!!
 
         } catch (SQLException e) {
             System.out.println("There was a SQL exception " + e);
